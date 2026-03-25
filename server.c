6c21cc3a4ee5e004828db9ea2cca300f6a2af1bb
zambeloago <zambeloago@gmail.com>
2026-03-25 01:04:04 -0400

    made makefile, started writing server code

---
+++ server.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int listen_socket = socket(AF_INET, SOCK_STREAM, 0);
