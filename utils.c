dfa571fe853cfe5e0ac55ec09f8cc6d6a9fe97ed
Konstantin Shtop <konstantin.shtop@mail.utoronto.ca>
2026-03-31 13:47:51 -0400

    Implemented utils.c: send_msg, trim_newline

--- utils.c
+++ utils.c
@@ -1,4 +1,6 @@
 #include "utils.h"
+#include "game.h"
+#include <errno.h>
 
 // "A5" -> row=4, col=0
 int parse_coords(const char *str, int *row, int *col) {
@@ -11,10 +13,30 @@ int parse_coords(const char *str, int *row, int *col) {
 void send_msg(int fd, const char *fmt, ...) {
     char buf[BUF_SIZE];
     va_list args;
-    // TODO: va_start, vsnprintf into buf, va_end
-    // TODO: loop send() until all bytes sent
+    va_start(args, fmt);
+    int len = vsnprintf(buf, sizeof(buf), fmt, args);
+    va_end(args);
+
+    if (len < 0) return;
+    if (len >= (int)sizeof(buf)) len = (int)sizeof(buf) - 1;
+
+    int sent = 0;
+    while (sent < len) {
+        ssize_t n = send(fd, buf + sent, (size_t)(len - sent), 0);
+        if (n < 0) {
+            if (errno == EINTR) continue;
+            return;
+        }
+        if (n == 0) return;
+        sent += (int)n;
+    }
 }
 
 void trim_newline(char *str) {
-    // TODO: strip trailing '\n' and '\r'
+    if (!str) return;
+    size_t n = strlen(str);
+    while (n > 0 && (str[n - 1] == '\n' || str[n - 1] == '\r')) {
+        str[n - 1] = '\0';
+        n--;
+    }
 }
